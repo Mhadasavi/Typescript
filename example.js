a665067b46f0d2e8c3d73004165f1b3b913236d2
Upkar <Upkar@DESKTOP-22L0J3T>
2022-03-07 19:20:11 +0530

    type annotation

--- example.js
+++ example.js
@@ -1,4 +1,13 @@
-function totalAmount(price, vat) {
-    console.log(price + vat);
+// function totalAmount(price,vat){
+//     console.log(price+vat);
+// }
+// totalAmount(100,20);
+//type annotations
+function add(price, vat) {
+    // console.log(price+vat);
+    return price + vat;
 }
-totalAmount(100, 20);
+console.log(add(3, 6));
+//variable annotations
+var name;
+name = 'uc';
--- example.ts
+++ example.ts
@@ -1,5 +1,16 @@
-function totalAmount(price,vat){
-    console.log(price+vat);
-}
+// function totalAmount(price,vat){
+//     console.log(price+vat);
+// }
 
-totalAmount(100,20);
+// totalAmount(100,20);
+
+//type annotations
+function add(price:number,vat:number):number{
+    // console.log(price+vat);
+    return price+vat;
+}
+console.log(add(3,6));
+
+//variable annotations
+let names :number ;
+names=5;
