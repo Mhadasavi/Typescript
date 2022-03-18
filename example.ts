912402ba38192f9564b612ea9469e3a5f7885efe
Upkar Chaudhary <hexaview9@whiteoakuk.com>
2022-03-18 22:39:41 +0530

    class, interface in typescript

--- example.ts
+++ example.ts
@@ -5,21 +5,41 @@
 // totalAmount(100,20);
 
 //type annotations
-function add(price:number,vat:number):number{
-    // console.log(price+vat);
-    return price+vat;
+function add(price: number, vat: number): number {
+  // console.log(price+vat);
+  return price + vat;
 }
-console.log(add(3,6));
+console.log(add(3, 6));
 
 //variable annotations
-let names :number ;
-names=5;
+let names: number;
+names = 5;
 
 //union types
-let names2: string|null=null;
+let names2: string | null = null;
 
-function add2(price:number,vat:number):number|boolean{
-    // console.log(price+vat);
-    return price+vat;
+function add2(price: number, vat: number): number | boolean {
+  // console.log(price+vat);
+  return price + vat;
 }
-console.log(add(3,6));
+console.log(add(3, 6));
+
+//interface
+interface IAccount {
+  name: string;
+  balance: number;
+  status?: string;
+  deposit?: () => void;
+}
+const Account: IAccount = {
+  name: "",
+  balance: 0,
+};
+
+let accounts: IAccount[];
+
+class InvestmentAccount implements IAccount {
+  //   balance;
+  //   name;
+  constructor(public balance, public name) {}
+}
